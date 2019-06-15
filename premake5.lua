a45469abd5caee9f1c6d160268868b744637b5ae
MorganDenes <40871074+MorganDenes@users.noreply.github.com>
2019-06-15 13:44:17 -0400

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Pixel"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+			"Debug",
+			"Release",
+			"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pixel"
+	location "Pixel"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.16299.0"
+
+		defines  
+		{
+			"PXL_PLATFORM_WINDOWS",
+			"PXL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			{"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"}
+		}
+
+	filter "configurations:Debug"
+		defines "PXL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PXL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PXL_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Pixel/vendor/spdlog/include",
+		"Pixel/src"
+	}
+
+	links
+	{
+		"Pixel"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.16299.0"
+
+		defines  
+		{
+			"PXL_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "PXL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PXL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PXL_DIST"
+		optimize "On"
